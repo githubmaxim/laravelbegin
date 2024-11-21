061dc48edf18622c516e02c308211b682afcfecc
GithubMax <githubmax@ukr.net>
2024-11-21 10:46:29 +0200

    Add new code in WorkWithArrayCommand.php

--- app/Console/Commands/WorkWithArrayCommand.php
+++ app/Console/Commands/WorkWithArrayCommand.php
@@ -23,25 +23,25 @@ class WorkWithArrayCommand extends Command
             ->limit(2)
 //            ->take(2) //или так
             ->get();
-        echo "query_items = ";
-        dump($query_items);
+//        echo "query_items = ";
+//        dump($query_items);
 
         $twoitems = [];
         foreach ($query_items as $item) {
-            $twoitems[$item->id] = [
+            $twoitems[$item->id] = [ //вставляем сюда ключ с номером "id", чтобы потом, при формировании нового массива, можно было вставлять эти значения без лишнего перебора внутри перебора
                 $item->SurfaceArea,
                 $item->created_at,
             ];
         }
-        echo "twoitems = ";
-        dump($twoitems);
+//        echo "twoitems = ";
+//        dump($twoitems);
 
-//        $data2 = ['id', 'Name', 'Code', 'SurfaceArea', 'created_at'];
-        $group_data = [
+        $group_data2 = ['id', 'Name', 'Code', 'SurfaceArea', 'created_at']; //так я просто заполняю массив ключами со значениями
+        $group_data = [ //так я создаю в массиве еще один массив(подмассив), и уже подмассив заполняю ключами со значениями. И этот подмассив будет с ключем "0".
             ['id', 'Name', 'Code', 'json']
         ];
-//        dump($data);
-//        dd($data2);
+//        dump($group_data);
+//        dd($group_data2);
 
         foreach ($query_items as $item) {
             $group_data [$item->id] = [ //нужно вставлять при формировании массива ключ, иначе просто будет заменяться одно значение на другое
@@ -52,23 +52,59 @@ class WorkWithArrayCommand extends Command
                 json_encode($twoitems[$item->id]), //так в массиве формируем строку, которая содержит JSON-представление значения (в квадратных скобках, через запятую, выбираются значения текущей строки массива)
             ];
         }
-        echo "group_data = ";
-        dump( $group_data);
+//        echo "group_data = ";
+//        dump( $group_data);
 
         $data = [
-            'camp_id' => 92,
+            'camp_id' => 92, //один раз, сверху, в массив добавляем данные этой и следующей строчки
             'brand_id' => 1,
             'csv_data' => array_values($group_data), //меняет мои ключи на ключи по порядку
 //            'csv_data' => $group_data,
         ];
-
-//        dd($data);
+//        echo "data = ";
+//        dump($data);
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-        $colum = $data['csv_data'][0]; //берем такой элемент из массива
-        dd($colum);
+        $column = $data['csv_data'][0]; //берем такой элемент из массива
+        unset($data['csv_data'][0]);
+        echo "column = ";
+        dump($column);
+//        echo "data = ";
+//        dd($data);
+$camp_id = $data['camp_id'];
+//        echo "camp_id = ";
+//        dd($camp_id);
 
+        $brand_id = $data['brand_id'];
+//        echo "brand_id = ";
+//        dd($brand_id);
+
+        $main_columns = ['id', 'name', 'surfacearea'];
+        $custom_column = [];
+        $a = 19;
+//        foreach ($column as $name) {
+        foreach ($column as $key => $name) {
+            $code = strtolower(str_replace(' ', '_', trim($name)));
+            echo "code = ";
+            dump($code);
+//            if (1>1) {
+            if (!in_array($code, $main_columns)) {
+                //если такое значение не найдено, то мы, методом "get_customfield" (реальная строчка кода закомментирована) запишем его в таблицу и полученный id-номер записи занесем (как значение) в массив "$custom_column"
+                $colar = array(
+                    'code' => $code,
+                    'title' => $name,
+                );
+//                $custom_column[$code] = $this->get_customfield($colar);
+                $custom_column[$code] = $a++;
+                echo "custom_column = ";
+                dump($custom_column);
+            }
+            //а если такое значение найдено, то мы перезаписываем его с внесенными правками(или без, если и так было так же) в массиве $column
+//            $column = $code; //если писать так, то будет просто менять одно значение на другое
+            $column[$key] = $code;
+            dump($column);
+        }
 
     }
 
